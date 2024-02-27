88700273525d6e6bb55269a6dc1d5cb1653cf76a
umpinny <161404091+umpinny@users.noreply.github.com>
2024-02-27 14:04:11 +0700

    Update account.service.js

--- cubank/frontend/src/services/account.service.js
+++ cubank/frontend/src/services/account.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:4000/api/v1/transactions/";
+
 
 const getAccount = (accountId) => {
   let data = JSON.parse(localStorage.getItem("user"));
